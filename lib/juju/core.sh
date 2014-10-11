d4ebe8f776f7c20e27c80888f8747e4fbd6a2921
Filippo Squillace <squillace.filippo@gmail.com>
2014-10-11 15:59:13 +0100

    Generate the package group

--- lib/juju/core.sh
+++ lib/juju/core.sh
@@ -22,7 +22,7 @@
 # https://wiki.archlinux.org/index.php/PKGBUILD
 # https://wiki.archlinux.org/index.php/Creating_Packages
 
-set -e
+#set -e
 
 FILE="$(readlink -f ${BASH_ARGV[0]})"
 
@@ -33,7 +33,7 @@ TAR=tar
 source "$(dirname ${BASH_ARGV[0]})/util.sh"
 
 ################################# VARIABLES ##############################
-FILIX_REPO=https://github.com/fsquillace/filix-repo/raw/master/package-groups
+JUJU_REPO=https://github.com/fsquillace/filix-repo/raw/master/package-groups
 [ -z $JUJU_PACKAGE_HOME ] && JUJU_PACKAGE_HOME=~/.juju
 
 ################################# MAIN FUNCTIONS ##############################
@@ -95,7 +95,7 @@ function install_package_group_from_repo(){
     maindir=$(TMPDIR=/tmp mktemp -d -t juju.XXXXXXXXXX)
 
     builtin cd ${maindir}
-    $WGET ${FILIX_REPO}/$(uname -m)/${pkggrp}.tar.gz
+    $WGET ${JUJU_REPO}/$(uname -m)/${pkggrp}.tar.gz
     $TAR -zxvf ${pkggrp}.tar.gz
 
     echo -e "\033[1;37mInstalling ${pkggrp}...\033[0m"
@@ -108,3 +108,121 @@ function install_package_group_from_repo(){
     return 0
 }
 
+
+function normalize_package(){
+    [ "$1" == "sh" ] && echo "bash" && return
+    [ "$1" == "libusbx" ] && echo "libusb" && return
+    echo $1 | sed -e 's/>=.*//' -e 's/>.*//' \
+        -e 's/<=.*//' -e 's/<.*//' \
+        -e 's/==.*//'
+}
+
+function suppress_duplicates(){
+    echo "${@}" | tr ' ' '\n' | sort -u | tr '\n' ' '
+}
+
+function get_closure_dependencies(){
+    # Calculate the closure dependencies of a package
+    #
+    # $1: pkgname (mandatory): str -name of the package
+
+    pkgname=$1
+    if [ -z "$pkgname" ]
+    then
+        echoerr -e "\033[1;31mError: Package name not specified\033[0m"
+    fi
+    normalized_pkgname=$(normalize_package $pkgname)
+
+    out=$(pacman -Qi $normalized_pkgname | grep "Depends On" | sed 's/Depends On.* ://')
+    deps=($out)
+    if [ "${deps[0]}" == "None" ] || [ "${deps}" == "" ]
+    then
+        echo $normalized_pkgname
+        return
+    fi
+
+    tot_deps=()
+    for dep in ${deps[@]}
+    do
+        new_deps=$(get_closure_dependencies $dep)
+        tot_deps=($(suppress_duplicates ${tot_deps[@]} ${new_deps[@]}))
+    done
+    echo $normalized_pkgname ${tot_deps[@]}
+}
+
+
+function generate_package_group(){
+    # Generate a package group from a package name
+    #
+    # $@: pkgname(s) (mandatory): str - Target package names
+
+    # Store the original working directory
+    local origin_wd=$(pwd)
+
+    if [ -z "$1" ]
+    then
+        echoerr -e "\033[1;31mError: Package name not specified\033[0m"
+        return 1
+    fi
+
+    # use mktemp, a non portable way is: $(mktemp --tmpdir=/tmp -d juju.XXXXXXXXXX)
+    maindir=$(TMPDIR=/tmp mktemp -d -t juju.XXXXXXXXXX)
+    mkdir -p ${maindir}/packages
+    mkdir -p ${maindir}/metadata
+    builtin cd ${maindir}/packages
+
+    for pkgname in $@
+    do
+        echo -e "\033[1;37mCalculating the closure dependencies for ${pkgname}...\033[0m"
+        deps=$(get_closure_dependencies $pkgname)
+        echo -e "\033[1;37mList of closure dependencies:\033[0m"
+        echo $deps
+        echo -e "\033[1;37mCopying the dependencies...\033[0m"
+        for dep in $deps
+        do
+            mkdir -p $dep
+            pacman -Ql $dep | grep -v "/$" | sed 's/.* //' | xargs -I {} bash -c "[ -f {} ] && cp --parents {} $dep"
+        done
+
+        if [ -e "${pkgname}/usr/bin" ]
+        then
+            mkdir -p ld
+            for executable in $(ls ${pkgname}/usr/bin/*)
+            do
+                echo -e "\033[1;37mCopying the dynamic libraries for ${executable}...\033[0m"
+                for lib in $( ldd ${executable} | grep -v dynamic | grep "=>" | awk '{print $3}' )
+                do
+                    [ -e $lib ] && cp --parents $lib ld
+                done
+                for lib in $( ldd ${executable} | grep -v dynamic | grep -v "=>" | awk '{print $1}' )
+                do
+                    [ -e $lib ] && cp --parents $lib ld
+                done
+
+                # ARCH amd64
+                if [ -f /lib64/ld-linux-x86-64.so.2 ]; then
+                cp --parents /lib64/ld-linux-x86-64.so.2 ld
+                fi
+
+                # ARCH i386
+                if [ -f  /lib/ld-linux.so.2 ]; then
+                cp --parents /lib/ld-linux.so.2 ld
+                fi
+            done
+        fi
+
+    done
+    for pkgname in $@
+    do
+        filename="$pkgname-$filename"
+    done
+    filename=$(echo ${filename} | sed 's/-$//')
+    echo -e "\033[1;37mGenerating the compressed file ${filename}.tar.gz...\033[0m"
+    $TAR -zcf ${origin_wd}/${filename}.tar.gz -C ${maindir} packages
+
+    echo -e "\033[1;37mPackage group generated successfully\033[0m"
+
+    builtin cd $origin_wd
+
+    return 0
+}
